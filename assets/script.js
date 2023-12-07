4021f6bf069a9f15ed81350e976c33bac30b4a4b
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2023-12-06 21:28:55 -0500

    made edits to html to support leaflet integration, relabeled lat and lng vars in js and continued work on distance calculator and map integration in js

--- assets/script.js
+++ assets/script.js
@@ -3,73 +3,77 @@ var openWeatherAPI = "73ae999c41edfcbe7e963963ee76ff49";
 var searchButton = document.getElementById("searchButton");
 var searchInput = document.getElementById("searchInput");
 
+// created functin to calculate distance 
+// added code for map
+function calcDistance(fireballs, city) {
+    var map = L.map('map', {
+        center: [51.505, -0.09],
+        zoom: 13
+    });
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    
+    for (var fireball of fireballs) {
+        var distance = map.distance(fireball, city);
+        console.log(distance);
+    }
+};
+
+
 //  FIREBALL API    https://ssd-api.jpl.nasa.gov/fireball.api
 
 //added function to retreive lat and lon information from api data
 function getCityGeoData() {
     var city = searchInput.value;
-    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=73ae999c41edfcbe7e963963ee76ff49' )
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-                var lat = data[0].lat;
-                console.log(lat);
-                var lon = data[0].lon;
-                console.log(lon);
+    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=73ae999c41edfcbe7e963963ee76ff49')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var latCity = data[0].lat;
+            console.log(latCity);
+            var lonCity = data[0].lon;
+            console.log(lonCity);
+            getFireballData({
+                lat: data[0].lat,
+                lng: data[0].lon,
+            });
         });
-    }
+}
 
-function getFireballData() {
+function getFireballData(cityLatLon) {
     fetch('https://www.anthonycooper.me/nasa')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
 
-// added map variable
-    var map = L.map('map', {
-    center: [51.505, -0.09],
-    zoom: 13
-    });
-    
-// script example to calculate distance
+            const fireballs = data.data.map(function (item) {
+                var [dt, , , latitude, latDir, longitude, lonDir] = item;
+                var lng = Number(longitude);
+                var lat = Number(latitude);
+                if (latDir === 'S') {
+                    lat = lat * -1;
+                }
+                if (lonDir === 'W') {
+                    lng = lng * -1;
+                }
+                console.log('DATE', dt);
+                console.log('LAT', lat);
+                console.log('LON', lng);
+                return { lat, lng };
+            });
 
- //   function distanceCalc(map) {
-
-   // event() {options: {…}, _handlers: Array(7), _layers: {…}, _zoomBoundLayers: {…}, _sizeChanged: false, …}
-   // map.distance({lat: -9.4, lng: 17.4}, {lat: 0, lng: 0});
-   // 2191363.956095504
-   // map.distance({lat: -9.4, lng: 17.4}, {lat: 41.878113, lng: -87.629799});
-   // 11945516.868386751
-   // }
-
-
-        const item = data.data.map(function(item) {
-            var [dt,,,latitude,latDir, longitude, lonDir] = item;
-            var lng = Number(longitude);
-            var lat = Number(latitude);
-            if (latDir === 'S') {
-                lat = lat * -1;
-            }
-            if (lonDir === 'W') {
-                lng = lng * -1;
-            }
-            console.log('DATE', dt);
-            console.log('LAT', lat);
-            console.log('LON', lng);
-            return { lat, lng }
+            calcDistance(fireballs, cityLatLon);
         });
-
-        console.log('ITEM', item);
-    });
-
-    [0][5]
 };
 //added event listener to search button to run getCityGeoData function
-    searchButton.addEventListener('click', function(event) {
-        event.preventDefault()
-        getCityGeoData();
-        getFireballData();
-       });
+searchButton.addEventListener('click', function (event) {
+    event.preventDefault()
+    getCityGeoData();
+});
