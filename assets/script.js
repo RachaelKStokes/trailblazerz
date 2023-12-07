932cfe842e7c29d218cacc1b5dc5612ab23ba290
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2023-12-06 22:54:49 -0500

    made lots of changes to html structure, css styles, and js in order to get map to appear properly on the page

--- assets/script.js
+++ assets/script.js
@@ -2,29 +2,31 @@
 var openWeatherAPI = "73ae999c41edfcbe7e963963ee76ff49";
 var searchButton = document.getElementById("searchButton");
 var searchInput = document.getElementById("searchInput");
+var map = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 13
+});
+var storedFireballData;
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 // created functin to calculate distance 
 // added code for map
 function calcDistance(fireballs, city) {
-    var map = L.map('map', {
-        center: [51.505, -0.09],
-        zoom: 13
-    });
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-
-    
+  
     for (var fireball of fireballs) {
+        console.log("1");
         var distance = map.distance(fireball, city);
         console.log(distance);
     }
+    map.panTo(new L.LatLng(city.lat, city.lng));
 };
 
 
-//  FIREBALL API    https://ssd-api.jpl.nasa.gov/fireball.api
+// rearranged and simplified existing code and continued work on distance calculators
 
 //added function to retreive lat and lon information from api data
 function getCityGeoData() {
@@ -46,30 +48,37 @@ function getCityGeoData() {
         });
 }
 
+function mapFireballData(data) {
+    return data.map(function (item) {
+        var [dt, , , latitude, latDir, longitude, lonDir] = item;
+        var lng = Number(longitude);
+        var lat = Number(latitude);
+        if (latDir === 'S') {
+            lat = lat * -1;
+        }
+        if (lonDir === 'W') {
+            lng = lng * -1;
+        }
+        console.log('DATE', dt);
+        console.log('LAT', lat);
+        console.log('LON', lng);
+         // var marker = L.marker([city.lat, city.lng]).update(marker);
+        return { lat, lng };
+    });
+}
+
 function getFireballData(cityLatLon) {
+    if (storedFireballData) {
+        return calcDistance(storedFireballData, cityLatLon);
+    }
     fetch('https://www.anthonycooper.me/nasa')
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-
-            const fireballs = data.data.map(function (item) {
-                var [dt, , , latitude, latDir, longitude, lonDir] = item;
-                var lng = Number(longitude);
-                var lat = Number(latitude);
-                if (latDir === 'S') {
-                    lat = lat * -1;
-                }
-                if (lonDir === 'W') {
-                    lng = lng * -1;
-                }
-                console.log('DATE', dt);
-                console.log('LAT', lat);
-                console.log('LON', lng);
-                return { lat, lng };
-            });
-
-            calcDistance(fireballs, cityLatLon);
+            const fireballs = mapFireballData(data.data);
+            storedFireballData = fireballs;
+            calcDistance(storedFireballData, cityLatLon);
         });
 };
 //added event listener to search button to run getCityGeoData function
